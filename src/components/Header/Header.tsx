70e76fa34c42891649b5035c65d19b119b7e6de4
gogost6 <georgi.stoilkov@grind.bg>
2022-08-14 14:43:11 +0300

    mobile navigation update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,44 @@
 import { NavLink } from 'react-router-dom';
 import DarkMode from '../../DarkMode';
 import './Header.css';
+import hamburger from '../../assets/hamburger.svg';
+import xIcon from '../../assets/x-icon.svg';
+
+import { MouseEvent, useRef, useState } from 'react';
 
 export default function Header() {
+    const [navRef, setNavRef] = useState('');
+
+    const OnClick = (e: MouseEvent<HTMLElement>) => {
+        if (e.target instanceof HTMLImageElement) {
+            const el = e.target;
+            if (el.classList.contains('hamburger')) {
+                if (el.src.includes('hamburger')) {
+                    el.src = xIcon;
+                } else {
+                    el.src = hamburger;
+                }
+            }
+        }
+        if (navRef == '') {
+            setNavRef('shown');
+        } else {
+            setNavRef('');
+        }
+    };
+
     return (
-        <header>
-            <nav>
+        <header onClick={OnClick}>
+            <NavLink
+                className={(isActive) =>
+                    'logo mobile nav-link' + (!isActive ? '' : ' active')
+                }
+                to="/"
+            >
+                GS.
+            </NavLink>
+            <img className="hamburger" src={hamburger} alt="hamburger" />
+            <nav className={navRef}>
                 <NavLink
                     className={(isActive) =>
                         'logo nav-link' + (!isActive ? '' : ' active')
@@ -46,8 +79,8 @@ export default function Header() {
                 >
                     contact
                 </NavLink>
+                <DarkMode />
             </nav>
-            <DarkMode />
         </header>
     );
 }
