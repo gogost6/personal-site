750036604657289e0247092ec5f677bd8813dae9
gogost6 <georgi.stoilkov@grind.bg>
2022-08-20 18:20:42 +0300

    works

--- src/components/Works/Works.tsx
+++ src/components/Works/Works.tsx
@@ -1,44 +1,34 @@
-import './Works.css';
+import './Works.scss';
 import saltImg from '../../assets/projects/salt.png';
-import { MouseEvent } from 'react';
+import { MouseEvent, useState } from 'react';
 
 export default function Works() {
     let imagesArray = [];
+    const categories = [
+        { id: 1, text: 'All' },
+        { id: 2, text: 'E-commerce' },
+        { id: 3, text: 'Coorperate' },
+        { id: 4, text: 'Mobile app' },
+    ];
+    const [activeId, setActiveId] = useState<number>(1);
 
     for (let i = 0; i < 10; i++) {
         imagesArray.push(saltImg);
     }
-    const onClick = (e: MouseEvent<HTMLElement>) => {
-        if (e.target instanceof HTMLElement) {
-            const el: HTMLElement = e.target;
-
-            if (el.tagName == 'A') {
-                const children = Array.from(e.currentTarget.children);
-                children.forEach((x) => {
-                    x.children[0].className = '';
-                });
-                el.className = 'active';
-            }
-        }
-    };
 
     return (
         <div className="works-container">
             <h1>Works page</h1>
             <h2>Repository of projects that I worked before.</h2>
-            <ul onClick={onClick}>
-                <li>
-                    <a className="active">All</a>
-                </li>
-                <li>
-                    <a>E-Commerce</a>
-                </li>
-                <li>
-                    <a>Coorperate</a>
-                </li>
-                <li>
-                    <a>Mobile app</a>
-                </li>
+            <ul>
+                {categories.map((x) => (
+                    <li
+                        onClick={() => setActiveId(x.id)}
+                        className={activeId === x.id ? 'active' : ''}
+                    >
+                        <a>{x.text}</a>
+                    </li>
+                ))}
             </ul>
             <div className="images-container">
                 {imagesArray.map((x) => {
